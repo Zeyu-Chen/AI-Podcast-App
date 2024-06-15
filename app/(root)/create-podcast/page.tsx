e890fdaf89b0d7bf17809ea6b5c3e17c0a7c7058
Zeyu Chen <iszeyu.chen@gmail.com>
2024-06-16 03:57:06 +1000

    feat(create-podcast): add input for title, select voice and description

--- app/(root)/create-podcast/page.tsx
+++ app/(root)/create-podcast/page.tsx
@@ -1,12 +1,153 @@
-import React from 'react';
+'use client';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from '@/components/ui/select';
+import { cn } from '@/lib/utils';
+import { useState } from 'react';
+import { Textarea } from '@/components/ui/textarea';
+
+const voiceCategories = ['alloy', 'shimmer', 'nova', 'echo', 'fable', 'onyx'];
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: 'Username must be at least 2 characters.'
+  })
+});
 
 const CreatePodcast = () => {
+  const [voiceType, setVoiceType] = useState<string | null>(null);
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: ''
+    }
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit (values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  // todo: add label for input, form, select...
   return (
-    <div>
+    <section className="mt-10 flex flex-col">
       <h1 className="text-20 font-bold text-white-1">
         Create Podcast
       </h1>
-    </div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="mt-12 flex w-full flex-col"
+        >
+          <div className="flex flex-col gap-[30px] border-b border-black-5 pb-10">
+            <FormField
+              control={form.control}
+              name="podcastTitle"
+              render={({field}) => (
+                <FormItem className="flex flex-col gap-2.5">
+                  <FormLabel className="text-16 font-bold text-white-1">
+                    Title
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Podcast Title"
+                      className="input-class focus-visible:ring-orange-1"
+                      autoComplete="username"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage className="text-white-1"/>
+                </FormItem>
+              )}
+            />
+
+            <div className="flex flex-col gap-2.5">
+              <Label className="text-16 font-bold text-white-1">
+                Select AI Voice
+              </Label>
+              <Select onValueChange={(value) => {
+                setVoiceType(value);
+              }}>
+                <SelectTrigger className={cn('w-full border-none bg-black-1 text-gray-1')}>
+                  <SelectValue
+                    placeholder="Select AI Voice"
+                    className="placeholder:text-gray-1"
+                  />
+                </SelectTrigger>
+                <SelectContent className="text-16 border-none bg-black01 font-bold text-white-1 focus:ring-orange-1">
+                  {voiceCategories.map(
+                    (category) => (
+                      <SelectItem
+                        key={category}
+                        value={category}
+                        className="capitalize focus:bg-orange-1"
+                      >
+                        {category}
+                      </SelectItem>
+                    )
+                  )}
+                </SelectContent>
+                {voiceType && (
+                  <audio
+                    src={`/${voiceType}.mp3`}
+                    autoPlay
+                    className="hidden"
+                  />
+                )}
+              </Select>
+            </div>
+
+            <FormField
+              control={form.control}
+              name="podcastDescription"
+              render={({field}) => (
+                <FormItem className="flex flex-col gap-2.5">
+                  <FormLabel className="text-16 font-bold text-white-1">
+                    Description
+                  </FormLabel>
+                  <FormControl>
+                    <Textarea
+                      placeholder="Write a short podcast description"
+                      className="input-class focus-visible:ring-orange-1"
+                      autoComplete="username"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage className="text-white-1"/>
+                </FormItem>
+              )}
+            />
+
+          </div>
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </section>
   );
 };
 
