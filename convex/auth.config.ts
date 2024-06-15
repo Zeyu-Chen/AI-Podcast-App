1dbe85c3f1ff51072f96173d320684cd335e2b82
Zeyu Chen <iszeyu.chen@gmail.com>
2024-06-16 02:26:44 +1000

    feat(auth): integrated clerk with convex

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://sweet-coyote-27.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig;
